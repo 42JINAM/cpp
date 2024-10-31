7c2dd21054561a1d94d360150755eb29d1787c12
42JINAM <jinam@made-f0Ar7s8.clusters.42paris.fr>
2024-10-31 16:43:40 +0100

    ch3

---
+++ AccelatedCpp/ch3/CalculatingFinalScoresOfStudents/solution.cpp
@@ -0,0 +1,42 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  // Write your solution here
+};
+
+int main() {
+  cout << "Please enter your name: ";
+  string name;
+  cin >> name;
+  cout << "Hello, " << name << "!" << endl;
+
+  cout << "Please enter your midterm and final exam grades: ";
+  double midterm, final;
+  cin >> midterm >> final;
+
+  cout << "Enter all your homework grades, "
+          "followed by end-of-file: ";
+
+  int count = 0;
+  double sum = 0;
+
+  double x;
+
+  while (cin >> x) {
+    ++count;
+    sum += x;
+  }
+
+  streamsize prec = cout.precision();
+  cout << "Your final grade is " << setprecision(3)
+       << 0.2 * midterm + 0.4 * final + 0.4 * sum / count << setprecision(prec)
+       << endl;
+  return 0;
+}
