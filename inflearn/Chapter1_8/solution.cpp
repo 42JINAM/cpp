2f6d804bc7bdf1f7eea3ccf3f6c3fc330060eab0
42JINAM <jinam@student.42seoul.kr>
2024-11-12 14:26:07 +0100

    operators

---
+++ inflearn/Chapter1_8/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  // Write your solution here
+};
+
+int main() {
+  int x = 2; // x is a variable, 2 is a literal
+  // -x unary operator
+  // x + 2 is an expression, + is a binary operator
+  int y = (x > 0) ? 1 : 2; // ternary operator
+}
