bbe5cbf9ce60259d877358b085a02cf705ac64f5
42JINAM <jinam@student.42.fr>
2024-11-14 15:04:10 +0100

    calaulator for students

--- AccelatedCpp/ch4/CalculatingTotalScoresofStudents/solution.cpp
+++ AccelatedCpp/ch4/CalculatingTotalScoresofStudents/solution.cpp
@@ -1,5 +1,8 @@
 #include <algorithm>
+#include <iomanip>
+#include <ios>
 #include <iostream>
+#include <istream>
 #include <map>
 #include <stdexcept>
 #include <vector>
@@ -32,4 +35,44 @@ double grade(double midterm, double final, const vector<double> &hw) {
   }
   return (grade(midterm, final, median(hw)));
 }
-int main() {}
+
+istream &read_hw(istream &in, vector<double> &hw) {
+  if (in) {
+    hw.clear();
+    double x;
+    while (in >> x) {
+      hw.push_back(x);
+    }
+    in.clear();
+  }
+  return in;
+}
+int main() {
+  cout << "Please enter your first name : ";
+  string name;
+  cin >> name;
+  cout << "Hello, " << name << "|" << endl;
+
+  cout << "Please enter your midterm and final exam grades: ";
+  double midterm, final;
+  cin >> midterm >> final;
+
+  cout << "Enter all your homework grades, "
+          "followed by end-of-file : ";
+  vector<double> homework;
+
+  read_hw(cin, homework);
+
+  try {
+    double final_grade = grade(midterm, final, homework);
+    streamsize prec = cout.precision();
+    cout << "Your final grade is " << setprecision(3) << final_grade
+         << setprecision(prec) << endl;
+  } catch (domain_error) {
+    cout << endl
+         << "You must enter your grades."
+            "Please try again."
+         << endl;
+  }
+  return 0;
+}
