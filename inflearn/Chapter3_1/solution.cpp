f992311170c3b5eb65ed59f275a120c2ed00d54b
42JINAM <jinam@student.42.fr>
2024-12-03 18:07:13 +0100

    chapter 3

---
+++ inflearn/Chapter3_1/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  // Write your solution here
+};
+
+int main() {
+  // associativity
+  int x = 4 + 2 * 3; // * > +
+  int y = 3 * 4 / 2; // left to right
+  // unary plus
+  int z = -3;
+  //
+  // int r = 1 + 2 + 3 * 4;
+  // int c = 9;
+  // int b = 0;
+  // int a = b = c;
+  // int t = --w + 5; (w - 1) +5
+  // a || b && c || d;
+  // (a || (b && c))||d;
+  return 0;
+}
