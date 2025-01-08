6ef070c62c2283b9c73714a878b18f9fb4ca2f03
42JINAM <jinam@student.42.fr>
2025-01-08 16:53:34 +0100

    auto

---
+++ inflearn/4/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+  // Write your solution here
+};
+
+auto add(int x, int y) -> int {
+	return x + y;
+}
+
+int main() {
+	auto a = 123;
+	auto b = 123.0;
+	auto c = 1 + 2.0;
+	return 0;
+}
